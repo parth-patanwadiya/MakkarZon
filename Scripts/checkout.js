9c7f6f54298380c9c24f4b03b12a0417925482ec
parth-patanwadiya <parthparthb9999@gmail.com>
2024-06-06 14:49:31 -0700

    Remove products from html when click delete

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -15,7 +15,7 @@ cart.forEach((cartItem)=>{
   });
 
   cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
         Delivery date: Tuesday, June 21
       </div>
@@ -99,7 +99,11 @@ document.querySelector('.js-order-summary')
 document.querySelectorAll('.js-delete-link')
   .forEach((link)=>{
     link.addEventListener('click', ()=>{
-        const productId = link.dataset.productId;
-        removeFromCart(productId);
+      const productId = link.dataset.productId;
+      removeFromCart(productId);
+
+      const container = document.querySelector(`.js-cart-item-container-${productId}`);
+
+      container.remove();
     });
   });
