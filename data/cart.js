07e1cbe15c5dcdcc84f56178bbfc132c84db1d8c
parth-patanwadiya <parthparthb9999@gmail.com>
2024-06-07 10:26:01 -0700

    Save data and generate HTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,13 @@
 export let cart = JSON.parse(localStorage.getItem('cart')) || [
   {
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 3
+    quantity: 3,
+    deliveryOptionId: '1'
   },
   {
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1
+    quantity: 1,
+    deliveryOptionId: '2'
   }
 ];
 
@@ -40,7 +42,8 @@ export function addToCart(productId){
   else{
     cart.push({
       productId,
-      quantity: Number(quantity)
+      quantity: Number(quantity),
+      deliveryOptionId: '1'
     });
   }
 
