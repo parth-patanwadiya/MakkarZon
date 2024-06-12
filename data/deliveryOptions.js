d3fb44814cd97d3ef8ff4f315b90b126f8119099
parth-patanwadiya <parthparthb9999@gmail.com>
2024-06-11 22:33:06 -0700

    Skipped weekend from delivery date

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -1,3 +1,21 @@
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+import isWeekend from '../Scripts/utils/weekend.js';
+
+export function calculateDeliveryDate(deliveryOption){
+  let remainingDays = deliveryOption.deliveryDays;
+  let deliveryDate = dayjs();
+
+  while(remainingDays > 0){
+    deliveryDate = deliveryDate.add(1, 'day');
+
+    if(!isWeekend(deliveryDate)){
+      remainingDays--;
+    }
+  }
+
+  return deliveryDate.format('dddd, MMMM D');
+}
+
 export function getDeliveryOption(deliveryOptionId){
   let deliveryOption;
 
