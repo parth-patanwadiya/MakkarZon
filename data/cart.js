27f321407c7dbaba7b233d062198e15aeef7212d
parth-patanwadiya <parthparthb9999@gmail.com>
2024-06-14 17:40:20 -0700

    Create automated Tests for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,15 +1,21 @@
-export let cart = JSON.parse(localStorage.getItem('cart')) || [
-  {
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 3,
-    deliveryOptionId: '1'
-  },
-  {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1,
-    deliveryOptionId: '2'
-  }
-];
+export let cart;
+
+loadFromStorage();
+
+export function loadFromStorage(){
+  cart = JSON.parse(localStorage.getItem('cart')) || [
+    {
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 3,
+      deliveryOptionId: '1'
+    },
+    {
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1,
+      deliveryOptionId: '2'
+    }
+  ];
+}
 
 function saveToStorage(){
   localStorage.setItem('cart',JSON.stringify(cart));
@@ -28,7 +34,7 @@ export function calculateCartQuantity(){
 export function addToCart(productId){
   let matchingItem;
 
-  const quantity = document.querySelector(`.js-quantity-selector-${productId}`).value;
+  // const quantity = document.querySelector(`.js-quantity-selector-${productId}`).value;
 
   cart.forEach((cartItem)=>{
     if(productId === cartItem.productId){
@@ -37,12 +43,12 @@ export function addToCart(productId){
   });
 
   if(matchingItem){
-    matchingItem.quantity += Number(quantity);
+    matchingItem.quantity += 1;
   }
   else{
     cart.push({
       productId,
-      quantity: Number(quantity),
+      quantity: 1,
       deliveryOptionId: '1'
     });
   }
--- tests-jasmine/utils/moneyTest.js
+++ tests-jasmine/utils/moneyTest.js
@@ -1,4 +1,4 @@
-import { formatCurrency } from "../Scripts/utils/money.js";
+import { formatCurrency } from "../../Scripts/utils/money.js";
 
 describe('Test suite: formatCurrency', () => {
   it('Converts cents into dollars', () => {
